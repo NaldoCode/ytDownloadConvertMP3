e2ed0f2699b88e5cc694a18f4f2051be09271fd5
Naldo Santos <enaldosantos94@gmail.com>
2020-05-13 23:04:50 -0300

    first commit

---
+++ ytDownloadConvertMP3/FrmDownloadYouTube.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ytDownloadConvertMP3
+{
+    partial class FrmDownloadYouTube
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDownloadYouTube));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.cBoxDelete = new System.Windows.Forms.CheckBox();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(10)))), ((int)(((byte)(10)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnMinimize);
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 27);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // btnClose
+            // 
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.btnClose.Location = new System.Drawing.Point(424, 1);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(23, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.btnMinimize.Location = new System.Drawing.Point(395, 1);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(23, 23);
+            this.btnMinimize.TabIndex = 5;
+            this.btnMinimize.Text = "-";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.label1.Location = new System.Drawing.Point(122, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Download YouTube Convert MP3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "URL:";
+            // 
+            // txtURL
+            // 
+            this.txtURL.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.txtURL.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtURL.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.txtURL.Location = new System.Drawing.Point(50, 45);
+            this.txtURL.MaxLength = 500;
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(385, 15);
+            this.txtURL.TabIndex = 1;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDownload.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.btnDownload.Location = new System.Drawing.Point(358, 112);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(80, 26);
+            this.btnDownload.TabIndex = 2;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // cBoxDelete
+            // 
+            this.cBoxDelete.AutoSize = true;
+            this.cBoxDelete.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.cBoxDelete.Location = new System.Drawing.Point(15, 121);
+            this.cBoxDelete.Name = "cBoxDelete";
+            this.cBoxDelete.Size = new System.Drawing.Size(89, 17);
+            this.cBoxDelete.TabIndex = 3;
+            this.cBoxDelete.Text = "Delete Vídeo";
+            this.cBoxDelete.UseVisualStyleBackColor = true;
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoading.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.lblLoading.Location = new System.Drawing.Point(45, 79);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(107, 25);
+            this.lblLoading.TabIndex = 5;
+            this.lblLoading.Text = "Loading...";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FrmDownloadYouTube
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.ClientSize = new System.Drawing.Size(450, 150);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.cBoxDelete);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmDownloadYouTube";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Download YouTube Convert MP3";
+            this.Load += new System.EventHandler(this.FrmDownloadYouTube_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.CheckBox cBoxDelete;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ ytDownloadConvertMP3/FrmDownloadYouTube.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using VideoLibrary;
+using MediaToolkit;
+using MediaToolkit.Model;
+using System.Windows.Forms;
+
+namespace ytDownloadConvertMP3
+{
+    public partial class FrmDownloadYouTube : Form
+    {
+        public FrmDownloadYouTube()
+        {
+            InitializeComponent();
+        }
+
+        int x;
+        int y;
+        bool mDown;
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        #region Move Panel :)
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+            mDown = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mDown)
+                SetDesktopLocation(MousePosition.X - x, MousePosition.Y - y);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mDown = false;
+        }
+        #endregion
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Opacity > 0)
+            {
+                Opacity -= 0.035;
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            if (txtURL.Text.Length > 10)
+            {
+                lblLoading.Text = "Loading...wait!";
+
+                string userName = Environment.UserName;
+                var path = $@"C:\Users\{userName}\Videos\"; // -.-
+                var youTube = YouTube.Default;
+
+                var video = youTube.GetVideo(txtURL.Text);
+                File.WriteAllBytes(path + video.FullName, video.GetBytes());
+
+                var inputFile = new MediaFile { Filename = path + video.FullName };
+                var outputFile = new MediaFile { Filename = $"{path + video.FullName}.mp3" };
+
+                using (var engine = new Engine())
+                {
+                    engine.GetMetadata(inputFile);
+                    engine.Convert(inputFile, outputFile);
+                }
+
+                lblLoading.Text = "Download Complete.";
+
+                if (cBoxDelete.Checked == true)
+                {
+                    File.Delete(path + video.FullName);
+                    lblLoading.Text = "Download Complete. | [Deleted Vídeo]";
+                }
+            }
+            else
+            {
+                lblLoading.Text = "Enter the URL!";
+            }
+        }
+        private void FrmDownloadYouTube_Load(object sender, EventArgs e)
+        {
+            txtURL.Select();
+        }
+    }
+}
